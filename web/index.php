3572cfdb6753ef2ef98623f2aed227680ac03943
kgapholad <kgapholad@yahoo.com>
2020-08-19 12:57:30 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -23,3 +23,8 @@ $app->get('/', function() use($app) {
 });
 
 $app->run();
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
+});
